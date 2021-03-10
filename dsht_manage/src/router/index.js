cceb415a31e80eeea688b2e93dc806c329a7e3aa
iw <918906131@qq.com>
2021-03-10 20:48:06 +0800

    'ss'

--- dsht_manage/src/router/index.js
+++ dsht_manage/src/router/index.js
@@ -3,16 +3,62 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/Home.vue'),
   },
   {
-    path: '/',
+    path: '/home',
+    name: 'Home',
+    component: () => import( /* webpackChunkName: "about" */ '../views/Home.vue'),
+    children: [{
+        path: '/',
+        redirect: '/home/userList'
+      },
+      {
+        path: 'users',
+        component: () => import('./../components/userList.vue')
+      },
+      {
+        path: 'roles',
+        component: () => import('./../components/addUser.vue')
+      },
+      {
+        path: 'rights',
+        component: () => import('./../components/removeUser.vue')
+      },
+      {
+        path: 'goods',
+        component: () => import('./../components/goods.vue')
+      },
+      {
+        path: 'params',
+        component: () => import('./../components/params.vue')
+      },
+      {
+        path: 'categories',
+        component: () => import('./../components/categories.vue')
+      },
+      {
+        path: 'orders',
+        component: () => import('./../components/orders.vue')
+      },
+      {
+        path: 'reports',
+        component: () => import('./../components/reports.vue')
+      },
+    ]
+  },
+  {
+    path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/login.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import( /* webpackChunkName: "about" */ '../views/login.vue')
   }
 ]
 
